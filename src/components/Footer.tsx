2c9723b2d2e82ea052a89b8b6d92d7c2202296eb
Cozma Rares <cozmarares11@gmail.com>
2023-09-24 01:53:43 +0300

    refactor: reword footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => (
-  <footer className="mx-auto mb-10 w-3/4 max-w-[50rem] text-center text-gray-500">
+  <footer className="mx-auto mb-10 w-3/4 max-w-[30rem] text-center text-gray-500">
     <p className="text-xs">
       <span className="font-semibold">About this website:</span> built with
-      React &amp; Next.js (App Router &amp; Server Actions), TypeScript,
-      Tailwind CSS, Framer Motion, React Email &amp; Resend, Vercel hosting.
+      React &amp; Next.js, TypeScript, Tailwind CSS, Framer Motion, React Email
+      &amp; Resend, deployed with Vercel.
     </p>
   </footer>
 );
