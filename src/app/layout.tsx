bf45c62682bdb6362fcc600e6214ac067edd7b5f
Cozma Rares <cozmarares11@gmail.com>
2023-10-01 15:48:46 +0300

    refactor: metadate generation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import ThemeSwitch from "@/components/ThemeSwitch";
 import Header from "@/components/Header";
 import MotionConfigWrapper from "@/components/MotionConfigWrapper";
@@ -7,21 +6,19 @@ import ActiveSectionContextProvider from "@/context/active-section";
 import { defaultFont } from "@/lib/fonts";
 import { Toaster } from "@/components/shadcn/ui/toaster";
 import Footer from "@/components/Footer";
+import { createMetadata } from "@/lib/utils";
 
-const author = "Rares Cozma";
-const name = "Personal Portfolio";
-const title = `${author} | ${name}`;
-const description =
-  "Discover the skills of Rares Cozma in this personal portfolio. With proficiency in JavaScript, React, and also back-end languages such as C/C++, and Java, along with a solid grasp of Git for version control.";
-const url = "https://raru.dev";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(url),
-  title,
-  description,
-  applicationName: name,
-  authors: { name: author },
-  generator: "Next.js",
+export const metadata = createMetadata({
+  title: "Rares Cozma | Personal Portfolio",
+  description:
+    "Discover the skills of Rares Cozma in this personal portfolio. With proficiency in JavaScript, React, and also back-end languages such as C/C++, and Java, along with a solid grasp of Git for version control.",
+  url: new URL("https://raru.dev"),
+  author: "Rares Cozma",
+  image: "",
+  themes: [
+    { theme: "light", color: "#f3f4f6" },
+    { theme: "dark", color: "#111827" },
+  ],
   keywords: [
     "Rares Cozma",
     "Cozma Rares",
@@ -31,26 +28,7 @@ export const metadata: Metadata = {
     "react",
     "nextjs",
   ],
-  themeColor: [
-    { media: "(prefers-color-scheme: dark)", color: "#111827" },
-    { media: "(prefers-color-scheme: light)", color: "#f3f4f6" },
-  ],
-  colorScheme: "light dark",
-  viewport: "width=device-width, initial-scale=1",
-  creator: author,
-  publisher: "Vercel",
-  alternates: { canonical: url },
-  robots: "index, follow",
-  openGraph: {
-    type: "website",
-    url,
-    title,
-    description,
-    siteName: name,
-  },
-  appleWebApp: true,
-  category: "web development",
-};
+});
 
 type Props = { children: React.ReactNode };
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import type { Metadata } from "next/types";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,4 +15,55 @@ export function getErrorMessage(error: unknown): string {
   if (typeof error === "string") return error;
 
   return "Something went wrong. Please try again.";
+}
+
+type ColorEnum = "light" | "dark" | "light dark" | "dark light";
+
+export function createMetadata({
+  title,
+  description,
+  url,
+  author,
+  image,
+  themes,
+  keywords,
+  noIndex = false,
+}: {
+  title: string;
+  description: string;
+  author: string;
+  url: URL;
+  image: string;
+  themes: Array<{ theme: "light" | "dark"; color: string }>;
+  keywords: string[];
+  noIndex?: boolean;
+}): Metadata {
+  const themeColor = themes.map(({ theme, color }) => ({
+    media: `(prefers-color-scheme: ${theme})`,
+    color,
+  }));
+  const colorScheme = themes.map(({ theme }) => theme).join(" ") as ColorEnum;
+
+  return {
+    metadataBase: url,
+    title,
+    description,
+    applicationName: title,
+    authors: { name: author },
+    keywords,
+    themeColor,
+    colorScheme,
+    viewport: "width=device-width, initial-scale=1",
+    creator: author,
+    openGraph: {
+      type: "website",
+      url,
+      title,
+      description,
+      siteName: title,
+      images: [{ url: image }],
+    },
+    appleWebApp: true,
+    robots: { index: !noIndex, follow: !noIndex },
+  };
 }
